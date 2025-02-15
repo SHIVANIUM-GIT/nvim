963a9d8a8e6024f80f71464edd22c5f3fa1c42e7
Shiv Rathore <shivrathore223@gmail.com>
2025-02-15 21:33:14 +0530

    update plugins/init.lua

--- lua/shiv/plugins/init.lua
+++ lua/shiv/plugins/init.lua
@@ -1,10 +1,3 @@
 return {
     { "nvim-tree/nvim-tree.lua", config = function() require("shiv.plugins.nvim-tree") end },
-    -- { "nvim-telescope/telescope.nvim", config = function() require("shiv.plugins.telescope") end },
-    -- { "neovim/nvim-lspconfig", config = function() require("shiv.plugins.lsp") end },
-    -- { "hrsh7th/nvim-cmp", config = function() require("shiv.plugins.cmp") end },
-    -- { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", config = function() require("shiv.plugins.treesitter") end },
-    -- { "lewis6991/gitsigns.nvim", config = function() require("shiv.plugins.git") end },
-    -- { "nvim-lualine/lualine.nvim", config = function() require("shiv.plugins.lualine") end },
-    -- { "windwp/nvim-autopairs", config = function() require("shiv.plugins.autopairs") end },
 }
