3a1d297c3f444dcddb33e77712380d4cec913a42
SHIV RATHORE <shivrathore223@gmail.com>
2026-02-04 14:24:07 +0530

    delete the lazy-lock.json

--- lua/shiv/plugins/telescope.lua
+++ lua/shiv/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
