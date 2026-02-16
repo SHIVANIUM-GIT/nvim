80a1e83c14b0cc445e452bc07340ecd6e56e3f77
SHIV RATHORE <shivrathore223@gmail.com>
2026-02-16 16:20:11 +0530

    fix: update lazy-lock.json with correct plugin commits; restore treesitter configuration

--- lua/shiv/plugins/treesitter.lua
+++ lua/shiv/plugins/treesitter.lua
@@ -1,25 +1,13 @@
--- return {
---   "nvim-treesitter/nvim-treesitter",
---   build = ":TSUpdate",
---   config = function()
---     require("nvim-treesitter.configs").setup({
---       ensure_installed = {
---         "lua",
---         "python",
---         "javascript",
---         "json",
---         "bash",
---       },
---       highlight = {
---         enable = true,
---       },
---       indent = {
---         enable = true,
---       },
---     })
---   end,
--- }
-
-return{
-end
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" }, -- important!
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "bash", "python", "json" },
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
