18582d9b13f8f3bbc75ee8197f16c0f2081f2168
Shiv Rathore <shivrathore223@gmail.com>
2026-08-28 11:18:34 +0530

    update the Treesitter.lua branch master

--- lua/shiv/plugins/treesitter.lua
+++ lua/shiv/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" }, -- important!
+
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "lua", "bash", "python", "json" },
