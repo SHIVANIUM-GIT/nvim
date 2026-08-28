ae67c47e77e820cb75af890c2906255782f7b3a1
Shiv Rathore <shivrathore223@gmail.com>
2026-08-28 14:49:05 +0530

    ADD: install : true

--- lua/shiv/plugins/treesitter.lua
+++ lua/shiv/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "lua", "bash", "python", "json", "cpp" },
+      auto_install = true,
       highlight = {
         enable = true,
       },
