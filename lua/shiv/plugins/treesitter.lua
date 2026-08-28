4dfe23996a3043a7d808f049e5889b26f39d1a27
Shiv Rathore <shivrathore223@gmail.com>
2026-08-28 11:51:13 +0530

    add CPP in treesitter

--- lua/shiv/plugins/treesitter.lua
+++ lua/shiv/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "bash", "python", "json" },
+      ensure_installed = { "lua", "bash", "python", "json", "cpp" },
       highlight = {
         enable = true,
       },
